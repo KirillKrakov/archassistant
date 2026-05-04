d488401b382174cc1bffa46e29e5a2a1d48c38e8
Kirill Krakov <kirill170251@gmail.com>
2026-05-04 15:46:31 +0300

    return back (second service update)

--- archassistant-backend/bin/main/com/example/archassistant/service/rules/ProfileRuleStrategy.kt
+++
@@ -1,9 +0,0 @@
-package com.example.archassistant.service.rules
-
-import com.example.archassistant.model.ArchitecturalRule
-import com.example.archassistant.model.ProjectProfile
-
-interface ProfileRuleStrategy {
-    val profile: ProjectProfile
-    fun generate(context: RuleGenerationContext): List<ArchitecturalRule>
-}
